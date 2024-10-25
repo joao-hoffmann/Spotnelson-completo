aa34b957e0f070afdccd6da43568f583fb546ba6
joao-hoffmann <162444512+joao-hoffmann@users.noreply.github.com>
2024-10-25 08:44:27 -0300

    Add files via upload

---
+++ html/index.js
@@ -0,0 +1,3 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+    alert('Redirecionando para a página de login...');
+});
---
+++ html/login.js
@@ -0,0 +1,12 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    console.log('E-mail:', email);
+    console.log('Senha:', password);
+
+    alert('Login realizado com sucesso!'); 
+    window.location.href = "./home.html"
+});
